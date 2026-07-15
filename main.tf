cff532d845bf587e6691752a36505c1da1188ff9
Mac <mac@Macs-MacBook-Pro.local>
2026-07-15 09:33:03 +0530

    switching to git ref

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "poc" {
-  source          = "./poc"
+  # source          = "./poc"
+  source          = "git::https://github.com/ashishkumar256/atlantis-demo.git//poc?ref=master"
   info            = local.sample
 }
