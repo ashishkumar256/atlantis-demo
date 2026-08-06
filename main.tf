84b37098e866200d170b74125d222622e8efaf94
Ashish Kumar <ashishkumar@macbook.local>
2026-08-06 12:19:54 +0530

    debug

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
-module "nginx" {
-  # source          = "./modules"
-  source          = "git::https://github.com/ashishkumar256/atlantis-demo.git//modules?ref=master"
-  info            = local.info
+module "service" {
+  # source              = "./modules"
+  source              = "git::https://github.com/ashishkumar256/atlantis-demo.git//modules?ref    =master"
+  for_each            = local.info
+  namespace_name      = each.value.namespace_name
+  deployment_name     = each.value.deployment_name
+  app_labels          = each.value.app_labels
+  replica_count       = each.value.replica_count
+  nginx_image         = each.value.nginx_image
+  container_resources = each.value.container_resources
+  service_config      = each.value.service_config
+  hpa_max_replicas    = each.value.hpa_max_replicas
+  hpa_metrics         = each.value.hpa_metrics
 }
